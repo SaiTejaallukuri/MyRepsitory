9e89bf7fe36135c81bc166fe0b462aa1d5d80613
alluk <alluk@LAPTOP-APQI475O>
2024-06-29 15:30:00 +0530

    Sending the selenium project

---
+++ src/main/java/Practice/Practice_1/App.java
@@ -0,0 +1,7 @@
+package Practice.Practice_1;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Practice/Practice_1/Launch_Browser.java
@@ -0,0 +1,28 @@
+package Practice.Practice_1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import bsh.Remote;
+
+public class Launch_Browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	WebDriverManager.chromeDriver().Setup();
+
+//		RemoteWebDriver driver= new ChromeDriver();
+		
+//		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+		
+		driver.manage().window().maximize();
+		
+		
+
+	}
+
+}
